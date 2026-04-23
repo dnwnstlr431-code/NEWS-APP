34aeaa462088962556c7901f61cf3349875a280a
dnwnstlr431-code <dnwnstlr431@gmail.com>
2026-04-24 08:52:08 +0900

    Update news.js

--- api/news.js
+++ api/news.js
@@ -96,11 +96,11 @@ module.exports = async (req, res) => {
             originalContent: originalText,
             analysis: analysis,
             source: 'Yahoo Finance',
-            publishedAt: (() => {
+            publishedAt: ( => {
             const d = new Date(article.pubDate);
             return `${d.getFullYear()}. ${String(d.getMonth()+1).padStart(2,'0')}. ${String(d.getDate()).padStart(2,'0')}. ${String(d.getHours()).padStart(2,'0')}:${String(d.getMinutes()).padStart(2,'0')}`;
-            })(),
             }),
+    
             url: article.link
           };
         } catch (err) {
